3506fd65849287876a03490b4f317e72dc1e9010
ztereohype <57519662+ZtereoHYPE@users.noreply.github.com>
2022-11-20 22:37:25 +0100

    change: fix namespace and modid

--- src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
@@ -1,8 +1,11 @@
 package codes.ztereohype.mtbackpackspreview;
 
+import lombok.Getter;
 import net.fabricmc.api.ModInitializer;
 
 public class MTBackpacksPreview implements ModInitializer {
+	private static final @Getter String MODID = "mtbprev";
+
 	@Override
 	public void onInitialize() {}
 }
