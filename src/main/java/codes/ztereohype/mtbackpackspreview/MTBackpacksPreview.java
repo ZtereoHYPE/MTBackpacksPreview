97d5cb8f58cf464e43e04d1a439741288f5d9194
ztereohype <57519662+ZtereoHYPE@users.noreply.github.com>
2022-11-20 22:45:39 +0100

    change: dont use getter for mod id

--- src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
@@ -1,10 +1,9 @@
 package codes.ztereohype.mtbackpackspreview;
 
-import lombok.Getter;
 import net.fabricmc.api.ModInitializer;
 
 public class MTBackpacksPreview implements ModInitializer {
-	private static final @Getter String MODID = "mtbprev";
+	public static final String MODID = "mtbprev";
 
 	@Override
 	public void onInitialize() {}
