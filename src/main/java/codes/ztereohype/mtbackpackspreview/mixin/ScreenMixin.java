3c0964f06edf00d8e4c45c165862be4608661735
masecla22 <supermasecla22@gmail.com>
2022-11-28 23:55:16 +0100

    Fixed up screen mixin to accomodate for proprer snapping

--- src/main/java/codes/ztereohype/mtbackpackspreview/mixin/ScreenMixin.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/mixin/ScreenMixin.java
@@ -20,9 +20,13 @@ import java.util.Optional;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
-    @Shadow public abstract List<Component> getTooltipFromItem(ItemStack itemStack);
-    @Shadow public int width;
+    @Shadow
+    public abstract List<Component> getTooltipFromItem(ItemStack itemStack);
 
+    @Shadow
+    public int width;
+    @Shadow
+    public int height;
 
     private ClientTooltipComponent tooltipComponent = null;
 
@@ -42,6 +46,8 @@ public abstract class ScreenMixin {
     void injectImageTooltip(PoseStack poseStack, ItemStack itemStack, int i, int j, CallbackInfo ci) {
         if (tooltipComponent != null) {
             int x = i + 12;
+            int y = j;
+
             int tooltipWidth = this.tooltipComponent.getWidth();
             for (FormattedCharSequence text : Lists.transform(this.getTooltipFromItem(itemStack), Component::getVisualOrderText)) {
                 int width = Minecraft.getInstance().font.width(text);
@@ -53,6 +59,17 @@ public abstract class ScreenMixin {
             if (x + tooltipWidth > this.width) {
                 x -= 28 + tooltipWidth;
             }
+
+            int o = 8 + this.tooltipComponent.getHeight();
+            int componentSize = this.getTooltipFromItem(itemStack).size();
+            if (componentSize > 1) {
+                o += 2 + (componentSize - 1) * 10;
+            }
+
+            if (j + o - 6 > this.height) {
+                j = this.height - o + 6;
+            }
+
             TooltipManager.renderTooltipComponent(poseStack, tooltipComponent, x, j);
         }
     }
@@ -64,13 +81,27 @@ public abstract class ScreenMixin {
 
     @ModifyConstant(method = "renderTooltip(Lcom/mojang/blaze3d/vertex/PoseStack;Ljava/util/List;II)V", constant = @Constant(intValue = 0, ordinal = 0))
     int injectTooltipWidth(int constant) {
-         return tooltipComponent != null ? constant + tooltipComponent.getWidth() : constant;
+        return tooltipComponent != null ? constant + tooltipComponent.getWidth() : constant;
+    }
+
+    private int forIndex = 0;
+
+    @ModifyArg(method = "renderTooltip(Lcom/mojang/blaze3d/vertex/PoseStack;Ljava/util/List;II)V",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;get(I)Ljava/lang/Object;")
+    )
+    public int captureLocals(int original) {
+        forIndex = original;
+        return original;
     }
 
     @ModifyVariable(method = "renderTooltip(Lcom/mojang/blaze3d/vertex/PoseStack;Ljava/util/List;II)V",
-                    at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/client/gui/Font;drawInBatch(Lnet/minecraft/util/FormattedCharSequence;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZII)I"),
-                    ordinal = 4)
-    int shiftDownardsRestOfTooltip(int value) {
-        return tooltipComponent != null ? value + tooltipComponent.getHeight() : value;
+            at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/client/gui/Font;drawInBatch(Lnet/minecraft/util/FormattedCharSequence;FFIZLcom/mojang/math/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;ZII)I"),
+            ordinal = 4)
+    int shiftDownardsRestOfTooltip(int original) {
+        if (this.tooltipComponent == null)
+            return original;
+        if (forIndex == 1)
+            return original + this.tooltipComponent.getHeight();
+        return original;
     }
 }
