8766a79486f1e83eed315c58a12eb07090d4ac42
masecla22 <supermasecla22@gmail.com>
2022-11-28 15:17:09 +0100

    Added modid to main class

--- src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
@@ -3,6 +3,9 @@ package codes.ztereohype.mtbackpackspreview;
 import net.fabricmc.api.ModInitializer;
 
 public class MTBackpacksPreview implements ModInitializer {
-	@Override
-	public void onInitialize() {}
+    public static final String MODID = "modid";
+
+    @Override
+    public void onInitialize() {
+    }
 }
