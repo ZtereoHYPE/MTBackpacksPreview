22ca64399e4cc559ee7a66860da58ae81630b507
masecla22 <supermasecla22@gmail.com>
2022-11-29 12:49:01 +0100

    Moved snapping logic inside the ClientBackpackTooltip

--- src/main/java/codes/ztereohype/mtbackpackspreview/mixin/ScreenMixin.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/mixin/ScreenMixin.java
@@ -1,5 +1,6 @@
 package codes.ztereohype.mtbackpackspreview.mixin;
 
+import codes.ztereohype.mtbackpackspreview.tooltip.ClientBackpackTooltip;
 import codes.ztereohype.mtbackpackspreview.tooltip.TooltipManager;
 import codes.ztereohype.mtbackpackspreview.tooltip.interfaces.ClientTooltipComponent;
 import codes.ztereohype.mtbackpackspreview.tooltip.interfaces.TooltipComponent;
@@ -7,6 +8,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,60 +22,33 @@ import java.util.Optional;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
-    //        @Shadow public abstract List<String> getTooltipFromItem(ItemStack itemStack);
     @Shadow
     public int width;
 
     @Shadow
     public int height;
 
-    private ClientTooltipComponent tooltipComponent = null;
+    protected ClientTooltipComponent tooltipComponent = null;
+
     private int fieldParsed = 0;
 
     @Inject(at = @At("HEAD"), method = "renderTooltip(Lnet/minecraft/item/ItemStack;II)V")
     void getTooltipMetadata(ItemStack itemStack, int i, int j, CallbackInfo ci) {
         TooltipComponent potentialTooltip = TooltipManager.getCustomTooltip(itemStack);
-
         if (potentialTooltip == null) {
             tooltipComponent = null;
             return;
         }
-
         tooltipComponent = ClientTooltipComponent.create(potentialTooltip);
     }
 
     @Inject(at = @At("RETURN"), method = "renderTooltip(Lnet/minecraft/item/ItemStack;II)V")
     void injectImageTooltip(ItemStack itemStack, int i, int j, CallbackInfo ci) {
         if (tooltipComponent != null) {
-            int x = i + 12;
-            int y = j;
+            Pair<Integer, Integer> snappedCoordinates = tooltipComponent.snapCoordinates(itemStack, i, j);
 
-            int tooltipWidth = this.tooltipComponent.getWidth();
-
-            List<String> knownTooltip = itemStack.getTooltip(MinecraftClient.getInstance().player,
-                    MinecraftClient.getInstance().options.advancedItemTooltips);
-
-            for (String text : knownTooltip) {
-                int width = MinecraftClient.getInstance().textRenderer.getStringWidth(text);
-                if (width > tooltipWidth) {
-                    tooltipWidth = width;
-                }
-            }
-
-            if (x + tooltipWidth > this.width) {
-                x -= 28 + tooltipWidth;
-            }
-
-            int o = 8 + this.tooltipComponent.getHeight();
-            int componentSize = knownTooltip.size();
-            if (componentSize > 1) {
-                o += 2 + (componentSize - 1) * 10;
-            }
-
-            if (y + o - 6 > this.height) {
-                y = this.height - o + 6;
-            }
-
+            int x = snappedCoordinates.getLeft();
+            int y = snappedCoordinates.getRight();
             TooltipManager.renderTooltipComponent(tooltipComponent, x, y);
         }
     }
@@ -87,7 +62,6 @@ public abstract class ScreenMixin {
     int injectTooltipWidth(int constant) {
         return tooltipComponent != null ? constant + tooltipComponent.getWidth() : constant;
     }
-
 
     private int forIndex = 0;
 
