80e1fb879cca999f46d8ce49f71e68da990ee591
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-11-30 17:39:06 +0100

    cleanup: last stuff 2

--- src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
@@ -3,8 +3,6 @@ package codes.ztereohype.mtbackpackspreview;
 import net.fabricmc.api.ModInitializer;
 
 public class MTBackpacksPreview implements ModInitializer {
-    public static final String MODID = "modid";
-
     @Override
     public void onInitialize() {
     }
