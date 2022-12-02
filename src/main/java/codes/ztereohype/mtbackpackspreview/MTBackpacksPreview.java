4a42ce7b438bcca7363f40128fc8f8d87b2b22a2
masecla22 <supermasecla22@gmail.com>
2022-12-03 00:07:38 +0100

    Fixed an issue with the hardcoded id

--- src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
+++ src/main/java/codes/ztereohype/mtbackpackspreview/MTBackpacksPreview.java
@@ -3,6 +3,8 @@ package codes.ztereohype.mtbackpackspreview;
 import net.fabricmc.api.ModInitializer;
 
 public class MTBackpacksPreview implements ModInitializer {
+    public final static String MODID = "mtbpprev";
+
     @Override
     public void onInitialize() {
     }
